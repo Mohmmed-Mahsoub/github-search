218ff97036609565ce353d20ebee47831f4ec3d4
unknown <mohamedmahsoub181@gmail.com>
2020-12-20 21:12:39 +0200

    route between pages using react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Dashboard from "./pages/dashboard";
+import Login from "./pages/login";
+import NotFound from "./pages/notFound";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="/notFound" component={NotFound} />
+        <Redirect from="/" exact to="/dashboard" />
+        <Redirect to="/notFound" />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
