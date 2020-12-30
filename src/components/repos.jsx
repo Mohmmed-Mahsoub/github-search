5d73e4b3b7c03d6566174a68bcf74eb532d7a539
unknown <mohamedmahsoub181@gmail.com>
2020-12-30 22:44:03 +0200

    dynamic data for starts per language chart component by mockData and context

--- src/components/repos.jsx
+++ src/components/repos.jsx
@@ -9,15 +9,14 @@ const Repos = () => {
   //1-fetch repos array from context
   const { repos } = useContext(Context);
   //2-loop in repos arry using reduce function to calclate how many repos used with every language and save the output in a variable
-  const languages = repos.reduce((accumulator, currentValue) => {
-    //4-destructing language from currentValue(every obj in array)
-    const { language } = currentValue;
+  const languagesAndStart = repos.reduce((accumulator, currentValue) => {
+    //4-destructing language from currentValue(every obj in array) //destructing stargazers_count to calculate start per languge
+    const { language, stargazers_count } = currentValue;
     //5-if language = null
     if (!language) {
       //just return the accumulator (the total)(that means go to the next currentValue)
       return accumulator;
     }
-
     /*
     //6-if there is not a key in the accumulator obj = accumulator[language] >> ("css" for EX)
     if (!accumulator[language]) {
@@ -31,45 +30,78 @@ const Repos = () => {
 
     //6-if there is not a key in the accumulator obj = accumulator[language] >> ("css" for EX)
     if (!accumulator[language]) {
-      //crete a one and put it's value = obj in this shap
-      accumulator[language] = { lable: language, value: 1 };
+      //crete a one and put it's value = obj in this shap (adding start to calulate start per lang)
+      accumulator[language] = {
+        label: language,
+        value: 1,
+        stars: stargazers_count,
+      };
     } else {
       //7-if there is a key in the accumulator obj = accumulator[language] >> go to it and keep the shap of obj just update it's value by adding one
       /*
-      //destructing the accumulator[language] >> { lable: language, value: 1 } then update the value (value: accumulator[language].value + 1)
+      //destructing the accumulator[language] >> { label: language, value: 1 } then update the value (value: accumulator[language].value + 1)
       accumulator[language] = {
         ...accumulator[language],
         value: accumulator[language].value + 1,
       };
       */
-      //this waymore clear for me
+      //this way more clear for me
       accumulator[language] = {
-        lable: language,
+        label: language,
         value: accumulator[language].value + 1,
+        stars: accumulator[language].stars + stargazers_count,
       };
     }
     //return the accumulator or the total until now and go to the next currentValue
     return accumulator;
   }, {}); //3-this is the initial value it can be a number or obj or array(depens on what the output you want)...
   /*
-  the output >> {CSS: {lable: "CSS", value: 46}, HTML: {lable: "HTML", value: 14}, JavaScript: {lable: "JavaScript", value: 37} }
-  but you want in in this shap >> {CSS: {lable: "CSS", value: 46} HTML: {lable: "HTML", value: 14}, {lable: "JavaScript", value: 37}}
+  the output >> {CSS: {label: "CSS", value: 46}, HTML: {label: "HTML", value: 14}, JavaScript: {label: "JavaScript", value: 37} }
+  but you want in in this shap >> {CSS: {label: "CSS", value: 46} HTML: {label: "HTML", value: 14}, {label: "JavaScript", value: 37}}
   so you want just the value from each obj >> Object.values(languages) (but this puting these in array so you should destricting them >> ...Object.values(languages))
   then take the top five valuse using sort() and slice()
   */
-  //replace the hard coded dat with the data you got it from repo
-  const chartData = [...Object.values(languages)]
+  console.log(languagesAndStart);
+
+  //language data (replace the hard coded data with the data you got it from repo)
+  const chartLanguagesData = [...Object.values(languagesAndStart)]
     .sort(function (a, b) {
       return b.value - a.value;
     })
     .slice(0, 5);
 
+  //stars per language data
+  const starsPerLanguage = [...Object.values(languagesAndStart)]
+    .sort(function (a, b) {
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .slice(0, 5);
+  console.log(starsPerLanguage, chartLanguagesData);
+
+  const chartData = [
+    {
+      label: "Venezuela",
+      stars: "20",
+      value: "290",
+    },
+    {
+      label: "Saudi",
+      value: "260",
+    },
+    {
+      label: "Canada",
+      value: "180",
+    },
+  ];
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <LanguagesChart data={chartData} />
+        <LanguagesChart data={chartLanguagesData} />
         <MostPopularChart />
-        <StarsChart />
+        <StarsChart data={starsPerLanguage} />
         <MostForkedChart />
       </Wrapper>
     </section>
@@ -95,6 +127,5 @@ const Wrapper = styled.div`
     width: 100% !important;
     border-radius: var(--radius) !important;
   }import LanguagesChart from './charts/languagesChart';
-
 `;
 export default Repos;
