ff0c418f6f959e2ce5554523a2de95788f202008
unknown <mohamedmahsoub181@gmail.com>
2020-12-30 17:44:38 +0200

    dynamic data for language chart component by mockData and ontext

--- src/components/repos.jsx
+++ src/components/repos.jsx
@@ -6,41 +6,64 @@ import MostForkedChart from "./charts/mostForkedChart";
 import MostPopularChart from "./charts/mostPopularChart";
 import StarsChart from "./charts/starsChart";
 const Repos = () => {
+  //1-fetch repos array from context
   const { repos } = useContext(Context);
-  const chartData = [
-    {
-      label: "Venezuela",
-      value: "290",
-    },
-    {
-      label: "Saudi",
-      value: "260",
-    },
-    {
-      label: "Canada",
-      value: "180",
-    },
-    {
-      label: "Iran",
-      value: "140",
-    },
-    {
-      label: "Russia",
-      value: "115",
-    },
-    {
-      label: "UAE",
-      value: "100",
-    },
-    {
-      label: "US",
-      value: "30",
-    },
-    {
-      label: "China",
-      value: "30",
-    },
-  ];
+  //2-loop in repos arry using reduce function to calclate how many repos used with every language and save the output in a variable
+  const languages = repos.reduce((accumulator, currentValue) => {
+    //4-destructing language from currentValue(every obj in array)
+    const { language } = currentValue;
+    //5-if language = null
+    if (!language) {
+      //just return the accumulator (the total)(that means go to the next currentValue)
+      return accumulator;
+    }
+
+    /*
+    //6-if there is not a key in the accumulator obj = accumulator[language] >> ("css" for EX)
+    if (!accumulator[language]) {
+      //crete a one and put it's value = 1
+      accumulator[language] = 1;
+    } else {
+      //7-if there is a key in the accumulator obj = accumulator[language] >> go to it and update it's value by adding one(key = value >> accumulator[language] = accumulator[language] + 1)
+      accumulator[language] = accumulator[language] + 1;
+    }
+    */
+
+    //6-if there is not a key in the accumulator obj = accumulator[language] >> ("css" for EX)
+    if (!accumulator[language]) {
+      //crete a one and put it's value = obj in this shap
+      accumulator[language] = { lable: language, value: 1 };
+    } else {
+      //7-if there is a key in the accumulator obj = accumulator[language] >> go to it and keep the shap of obj just update it's value by adding one
+      /*
+      //destructing the accumulator[language] >> { lable: language, value: 1 } then update the value (value: accumulator[language].value + 1)
+      accumulator[language] = {
+        ...accumulator[language],
+        value: accumulator[language].value + 1,
+      };
+      */
+      //this waymore clear for me
+      accumulator[language] = {
+        lable: language,
+        value: accumulator[language].value + 1,
+      };
+    }
+    //return the accumulator or the total until now and go to the next currentValue
+    return accumulator;
+  }, {}); //3-this is the initial value it can be a number or obj or array(depens on what the output you want)...
+  /*
+  the output >> {CSS: {lable: "CSS", value: 46}, HTML: {lable: "HTML", value: 14}, JavaScript: {lable: "JavaScript", value: 37} }
+  but you want in in this shap >> {CSS: {lable: "CSS", value: 46} HTML: {lable: "HTML", value: 14}, {lable: "JavaScript", value: 37}}
+  so you want just the value from each obj >> Object.values(languages) (but this puting these in array so you should destricting them >> ...Object.values(languages))
+  then take the top five valuse using sort() and slice()
+  */
+  //replace the hard coded dat with the data you got it from repo
+  const chartData = [...Object.values(languages)]
+    .sort(function (a, b) {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
   return (
     <section className="section">
       <Wrapper className="section-center">
