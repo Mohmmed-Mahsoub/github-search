30a9415a1cb9e895533bb0296a202ebbc8629513
unknown <mohamedmahsoub181@gmail.com>
2021-01-05 17:06:49 +0200

    install axios and fetch userInfo data and replace the mockData for user

--- src/contexts/context.jsx
+++ src/contexts/context.jsx
@@ -2,13 +2,15 @@ import React, { createContext, useState } from "react";
 import mockUserInfo from "./mockData/mockUserInfo";
 import mockFollower from "./mockData/mockFollowers";
 import mockRepos from "./mockData/mockRepos";
+import axios from "axios";
 export const Context = createContext();
 const ContextProvider = (props) => {
   const [userInfo, setUserInfo] = useState(mockUserInfo);
   const [followers, setFollowers] = useState(mockFollower);
   const [repos, setRepos] = useState(mockRepos);
-  const fetchDataForNewUser = (x) => {
-    console.log(x);
+  const fetchDataForNewUser = async (user) => {
+    const { data } = await axios.get(`https://api.github.com/users/${user}`);
+    setUserInfo(data);
   };
 
   return (
